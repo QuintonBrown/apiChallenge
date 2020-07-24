4e6fcfe03383742a820c8ada81d034012582d728
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-07-24 11:51:59 -0400

    connected todo input to api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const api = express();
+api.use(bodyParser.json());
 
 //This will let our program access this directory even if we move the folder around our system. Cool.
 api.use(express.static(__dirname + '/public'));
 
+
+
+api.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('It works!');
+   });
+   
 api.listen(3000, () => {
   console.log('API up and running!');
 });
+
+
 
 // api.get('/', (req, res) => {
 //   console.log(req);
