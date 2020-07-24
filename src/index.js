da25884fe3430e179c4d47540275f0fa4f74593f
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-07-24 10:59:34 -0400

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const api = express();
+
+//This will let our program access this directory even if we move the folder around our system. Cool.
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
-api.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello, world!');
-});
+// api.get('/', (req, res) => {
+//   console.log(req);
+//   res.send('Hello, world!');
+// });
