c4cf4c96f3184c6f74cfca2d202ebbe85d604f89
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-07-27 08:21:59 -0400

    Step 11: Connect API to MySQL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Bonnilyn11',
+    // database: 'todo'
+});
+
+try {
+connection.connect();
+}   catch(e) {
+    console.log('Oops, Connection to mysql failed.');
+    console.log(e)
+}
 
 const api = express();
 api.use(bodyParser.json());
@@ -12,7 +26,14 @@ api.use(express.static(__dirname + '/public'));
 
 api.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('It works!');
+    
+    connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+     if (error) return res.json({ error: error });
+   connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+      if (error) return res.json({ error: error });
+   console.log(results);
+     });
+    });
    });
    
 api.listen(3000, () => {
